b5aee7b4a301bbfb81f63bf4a045349bfab03d7d
SerikTr <41553217+SerikTr@users.noreply.github.com>
2020-10-22 14:40:00 +0200

    admin page ready to publication

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyBpzDD0frDiiP02gHQ1yvUXyCvYbm4MEKY'
 };
